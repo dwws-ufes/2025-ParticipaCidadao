99d9fbbddc4ea7c0e6e45c2df69d3544a3ef4959
GustavooPerini <gustavooperini@gmail.com>
2025-05-28 16:30:06 -0300

    Configurando o ambiente de desenvolvimento

---
+++ participacidadao-backend/src/main/java/br/ufes/participacidadao/ParticipacidadaoApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.participacidadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipacidadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipacidadaoApplication.class, args);
+	}
+
+}
---
+++ participacidadao-backend/src/test/java/br/ufes/participacidadao/ParticipacidadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.participacidadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipacidadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

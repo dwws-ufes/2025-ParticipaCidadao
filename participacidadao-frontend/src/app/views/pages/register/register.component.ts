579a2f9635629c4cafe9f04ec178a0904d125d2b
Gabriel <asdfriskk@gmail.com>
2025-06-28 01:16:00 -0300

    adicinando mensagens de retorno para pagina de login e cadastro

--- participacidadao-frontend/src/app/views/pages/register/register.component.ts
+++ participacidadao-frontend/src/app/views/pages/register/register.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import User from '../../../models/User';
 import { UserService } from '../../../services/user.service';
 import { Router } from '@angular/router';
+
+// Custom validator for matching passwords
+export function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password')?.value;
+  const repeatPassword = control.get('repeatPassword')?.value;
+  return password === repeatPassword ? null : { passwordMismatch: true };
+}
 
 @Component({
   selector: 'app-register',
@@ -10,18 +17,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
-
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    cpf: new FormControl(''),
-    birthDate: new FormControl(''),
-    password: new FormControl(''),
-    repeatPassword: new FormControl('')
-  });
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    cpf: new FormControl('', [Validators.required]),
+    birthDate: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    repeatPassword: new FormControl('', [Validators.required])
+  }, { validators: passwordMatchValidator });
 
-  user?:User
+  user?: User;
 
   constructor(private userService: UserService, private router: Router) { }
 
@@ -40,5 +45,4 @@ export class RegisterComponent {
       }
     });
   }
-
 }

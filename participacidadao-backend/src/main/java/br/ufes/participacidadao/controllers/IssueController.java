aee6c5144efa52dea93e18f1c8281db84fc43c00
gabriel <101599265+wdw0@users.noreply.github.com>
2025-08-23 15:19:34 -0300

    Update IssueController.java

--- participacidadao-backend/src/main/java/br/ufes/participacidadao/controllers/IssueController.java
+++ participacidadao-backend/src/main/java/br/ufes/participacidadao/controllers/IssueController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufes.participacidadao.models.IssueModel;
+
 import br.ufes.participacidadao.repositories.IssueRepository;
+import br.ufes.participacidadao.services.LinkedDataService;
+import br.ufes.participacidadao.services.IssueRdfService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -17,12 +22,25 @@ import java.util.List;
 @RequestMapping("/issues")
 public class IssueController {
 
+
     @Autowired
     private IssueRepository issueRepository;
+
+    @Autowired
+    private LinkedDataService linkedDataService;
+
+    @Autowired
+    private IssueRdfService issueRdfService;
+
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/new")
     public IssueModel createIssue(@RequestBody IssueModel issueModel) {
+        // Enriquecer dados da cidade ao criar issue
+        if (issueModel.getCity() != null && !issueModel.getCity().isEmpty()) {
+            var dados = linkedDataService.enriquecerDadosCidade(issueModel.getCity());
+            issueModel.setDadosEnriquecidos(dados);
+        }
         return this.issueRepository.save(issueModel);
     }
 
@@ -30,5 +48,12 @@ public class IssueController {
     @GetMapping
     public List<IssueModel> getAllIssues() {
         return issueRepository.findAll();
+    }
+
+    @GetMapping(value = "/rdf", produces = "text/turtle")
+    public ResponseEntity<String> getAllIssuesRdf() {
+        List<IssueModel> issues = issueRepository.findAll();
+        String rdf = issueRdfService.issuesToRdf(issues);
+        return ResponseEntity.ok().contentType(MediaType.valueOf("text/turtle")).body(rdf);
     }
 }
